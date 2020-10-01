38653430478013da98b4560722ddb4894afe3a60
Ketlin Pedron <pedron.ketlin@gmail.com>
2020-10-01 16:41:34 -0400

    Start Cypress development
    Signed-off-by: Ketlin Pedron <pedron.ketlin@gmail.com>

---
+++ cypress/tests/ui/login.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Suite to run Login features', () => {
+    
+    it('NOME DO CASO DE TESTE', () => {
+       cy.visit('https://phptravels.com/demo')
+       cy.get('.yx-njv > .btn').click()
+    })
+
+})
